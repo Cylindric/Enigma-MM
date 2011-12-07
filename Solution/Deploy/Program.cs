a2282618fb316ed341461b325a04c6a0d472d5dd
Cylindric <mark@hanfordonline.co.uk>
2011-12-07 14:23:07 +0000

    Updated the deployment tool a bit

--- Solution/Deploy/Program.cs
+++ Solution/Deploy/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Threading;
+using Ionic.Zip;
 
 namespace Deploy
 {
@@ -24,6 +25,7 @@ namespace Deploy
             string projectRoot = Path.GetDirectoryName(buildRoot);
             string deployRoot = Path.Combine(projectRoot, "Deploy");
             string sourceRoot = Path.Combine(projectRoot, "Solution");
+            string stagingRoot = Path.Combine(deployRoot, "staging");
 
             // Clean and create the deployment root
             if (Directory.Exists(deployRoot))
@@ -31,19 +33,68 @@ namespace Deploy
                 Directory.Delete(deployRoot, true);
             }
             Directory.CreateDirectory(deployRoot);
+            Directory.CreateDirectory(stagingRoot);
+
+            // Create the empty directories
+            foreach (KeyValuePair<string, string> p in paths)
+            {
+                if (!Directory.Exists(p.Value))
+                {
+                    Directory.CreateDirectory(Path.Combine(stagingRoot, p.Value));
+                }
+            }
 
             // Copy the main deployment files
-            CopyFiles(buildRoot, deployRoot, "*.exe");
-            CopyFiles(buildRoot, deployRoot, "*.dll");
+            CopyFiles(buildRoot, stagingRoot, "Server.exe");
+            CopyFiles(buildRoot, stagingRoot, "LibNbt.dll");
+            CopyFiles(buildRoot, stagingRoot, "Ionic.Zip.Reduced.dll");
 
-            // Remove extra files
-            DeleteFiles(deployRoot, "*.vshost.exe");
-            DeleteFiles(deployRoot, "Bootstrap.exe");
-            DeleteFiles(deployRoot, "MinecraftSimulator.exe");
-            DeleteFiles(deployRoot, "Moq.dll");
-            DeleteFiles(deployRoot, "nunit.framework.dll");
-            DeleteFiles(deployRoot, "Test.NullCommand.exe");
-            DeleteFiles(deployRoot, "Tests.dll");
+            // Copy the default scheduler config
+            CopyFiles(Path.Combine(sourceRoot, "Server", "Engine", "Scheduler"), stagingRoot, "sample.scheduler.xml");
+
+            // Build the monolithic license readme
+            string readme = File.ReadAllText(Path.Combine(projectRoot, "readme.txt"));
+            string nbtLic = File.ReadAllText(Path.Combine(sourceRoot, "LibNbt", "libnbt.txt"));
+            string ionLic = File.ReadAllText(Path.Combine(sourceRoot, "Server", "Engine", "Ionic.txt"));
+
+            StringBuilder finalReadme = new StringBuilder();
+            finalReadme.AppendLine("##### Contents");
+            finalReadme.AppendLine("##1## Readme");
+            finalReadme.AppendLine("##2## LibNbt License");
+            finalReadme.AppendLine("##3## Ionic Zip License");
+            finalReadme.AppendLine("#####");
+            finalReadme.AppendLine("");
+            finalReadme.AppendLine("");
+            finalReadme.AppendLine("################################################################################");
+            finalReadme.AppendLine("##1## Readme");
+            finalReadme.AppendLine("################################################################################");
+            finalReadme.AppendLine(File.ReadAllText(Path.Combine(projectRoot, "readme.txt")));
+            finalReadme.AppendLine("");
+            finalReadme.AppendLine("");
+            finalReadme.AppendLine("################################################################################");
+            finalReadme.AppendLine("##2## LibNbt License");
+            finalReadme.AppendLine("################################################################################");
+            finalReadme.AppendLine(File.ReadAllText(Path.Combine(sourceRoot, "LibNbt", "libnbt.txt")));
+            finalReadme.AppendLine("");
+            finalReadme.AppendLine("");
+            finalReadme.AppendLine("################################################################################");
+            finalReadme.AppendLine("##3## Ionic Zip License");
+            finalReadme.AppendLine("################################################################################");
+            finalReadme.AppendLine(File.ReadAllText(Path.Combine(sourceRoot, "Server", "Engine", "Ionic.txt")));
+
+            using (StreamWriter file = new System.IO.StreamWriter(Path.Combine(stagingRoot, "readme.txt")))
+            {
+                file.WriteLine(finalReadme.ToString());
+            }
+
+            // zip everything up
+            using (ZipFile zip = new ZipFile())
+            {
+                zip.CompressionLevel = Ionic.Zlib.CompressionLevel.BestCompression;
+                zip.AddSelectedFiles("*", stagingRoot, "", true);
+                zip.Save(Path.Combine(deployRoot, "Enigma-MM.zip"));
+            }
+
         }
 
 
