f50e67189ef471259219404f14acf3e0c00bec3b
Cylindric <mark@hanfordonline.co.uk>
2011-12-07 17:40:25 +0000

    Bootstrap now gets the latest Overviewer from Git automatically

--- Solution/Bootstrap/Program.cs
+++ Solution/Bootstrap/Program.cs
@@ -36,78 +36,148 @@ namespace Bootstrap
                 }
             }
 
+
             // Get any external tools that we can
-            DownloadFile(
-                Path.Combine(paths["minecraft"], "minecraft_server.jar"),
-                "https://s3.amazonaws.com/MinecraftDownload/launcher/minecraft_server.jar",
-                "Minecraft"
-            );
+            Console.Write("Attempting to download the Minecraft server...");
+            try
+            {
+                DownloadFile(
+                    Path.Combine(paths["minecraft"], "minecraft_server.jar"),
+                    "https://s3.amazonaws.com/MinecraftDownload/launcher/minecraft_server.jar",
+                    "Minecraft"
+                );
 
-            DownloadFile(
-                Path.Combine(paths["overviewer"], "overviewer.exe"),
-                "https://github.com/downloads/overviewer/Minecraft-Overviewer/win86_32-v0.3.0-215-g8390ec4.zip",
-                "Overviewer"
-            );
+                Console.WriteLine("done.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR!");
+                Console.WriteLine("Failed to find and/or download");
+                Console.WriteLine(ex.Message);
+            }
 
-            DownloadFile(
-                Path.Combine(paths["c10t"], "c10t.exe"),
-                "http://toolchain.eu/minecraft/c10t/releases/c10t-1.9-windows-x86.zip",
-                "C10t"
-            );
 
-            // C10t extracts into a subfolder, so move the files up one level
-            PromoteDirectory(Path.Combine(paths["c10t"], "c10t-1.9-windows-x86"));
+            Console.Write("Attempting to download Overviewer...");
+            try
+            {
+                if (File.Exists(Path.Combine(paths["overviewer"], "overviewer.exe")))
+                {
+                    Console.Write("already present...");
+                }
+                else
+                {
+                    Console.Write("searching...");
+                    string apiurl = "https://api.github.com/repos/overviewer/Minecraft-Overviewer/downloads";
+                    WebRequest rw = WebRequest.Create(apiurl);
+                    WebResponse resp = rw.GetResponse();
+                    StreamReader sr = new StreamReader(resp.GetResponseStream());
+                    string r = sr.ReadToEnd();
+
+                    // Poor-man's JSON decode :)
+                    // We only really need the first HTML_URL value
+                    int urlStart = r.IndexOf("html_url\":\"") + 11;
+                    int urlEnd = r.IndexOf("\"", urlStart + 1);
+                    string overviewerUrl = r.Substring(urlStart, urlEnd - urlStart);
+
+                    DownloadFile(
+                        Path.Combine(paths["overviewer"], "overviewer.exe"),
+                        overviewerUrl,
+                        "Overviewer"
+                    );
+                }
+                Console.WriteLine("done.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR!");
+                Console.WriteLine("Failed to find and/or download");
+                Console.WriteLine(ex.Message);
+            }
+
+
+             Console.Write("Attempting to download C10t...");
+             try
+             {
+                 if (File.Exists(Path.Combine(paths["c10t"], "c10t.exe")))
+                 {
+                     Console.Write("already present...");
+                 }
+                 else
+                 {
+                     DownloadFile(
+                          Path.Combine(paths["c10t"], "c10t.exe"),
+                          "http://toolchain.eu/minecraft/c10t/releases/c10t-1.9-windows-x86.zip",
+                          "C10t"
+                      );
+
+                     // C10t extracts into a subfolder, so move the files up one level
+                     Console.Write("promoting...");
+                     PromoteDirectory(Path.Combine(paths["c10t"], "c10t-1.9-windows-x86"));
+                 }
+                 Console.WriteLine("done.");
+             }
+             catch (Exception ex)
+             {
+                 Console.WriteLine("ERROR!");
+                 Console.WriteLine("Failed to find and/or download");
+                 Console.WriteLine(ex.Message);
+             }
         }
+
 
         private static void PromoteDirectory(string extractRoot)
         {
-            foreach (string filename in Directory.GetFiles(extractRoot))
+            if (Directory.Exists(extractRoot))
             {
-                File.Move(filename, Path.Combine(Path.GetDirectoryName(Path.GetDirectoryName(filename)), Path.GetFileName(filename)));
+                foreach (string filename in Directory.GetFiles(extractRoot))
+                {
+                    File.Move(filename, Path.Combine(Path.GetDirectoryName(Path.GetDirectoryName(filename)), Path.GetFileName(filename)));
+                }
+                foreach (string dir in Directory.GetDirectories(extractRoot))
+                {
+                    Directory.Move(dir, Path.Combine(Path.GetDirectoryName(Path.GetDirectoryName(dir)), Path.GetFileName(dir)));
+                }
+                Directory.Delete(extractRoot);
             }
-            foreach (string dir in Directory.GetDirectories(extractRoot))
+            else
             {
-                Directory.Move(dir, Path.Combine(Path.GetDirectoryName(Path.GetDirectoryName(dir)), Path.GetFileName(dir)));
+                Console.WriteLine("ERROR!");
+                Console.Write("Cannot promote directory {0}: not found", extractRoot);
             }
-            Directory.Delete(extractRoot);
         }
+
 
         private static void DownloadFile(string filename, string url, string name)
         {
-            if (!File.Exists(filename))
+            if (File.Exists(filename))
             {
-                try
+                Console.Write("already present...");
+            }
+            else
+            {
+                string downloadpath = Path.Combine(Path.GetDirectoryName(filename), Path.GetFileName(url));
+                if (File.Exists(downloadpath))
                 {
-                    string downloadpath = Path.Combine(Path.GetDirectoryName(filename), Path.GetFileName(url));
-                    Console.Write("Attempting to download {0}...", name);
-                    if (File.Exists(downloadpath))
-                    {
-                        Console.WriteLine("download file already exists!");
-                    }
-                    else
-                    {
-                        WebClient web = new WebClient();
-                        web.DownloadFile(url, downloadpath);
-                        Console.WriteLine("done");
-                    }
-                    if (Path.GetExtension(downloadpath) == ".zip")
-                    {
-                        using (ZipFile zip = new ZipFile(downloadpath))
-                        {
-                            zip.ExtractAll(Path.GetDirectoryName(downloadpath), ExtractExistingFileAction.OverwriteSilently);
-                        }
-                    }
+                    Console.WriteLine("found...");
                 }
-                catch (Exception ex)
+                else
                 {
-                    Console.WriteLine("could not download!");
-                    Console.WriteLine(ex.Message);
+                    Console.Write("downloading...");
+                    WebClient web = new WebClient();
+                    web.DownloadFile(url, downloadpath);
+                    web.Proxy = null;
+                }
+                if (Path.GetExtension(downloadpath) == ".zip")
+                {
+                    Console.Write("extracting...");
+                    using (ZipFile zip = new ZipFile(downloadpath))
+                    {
+                        zip.ExtractAll(Path.GetDirectoryName(downloadpath), ExtractExistingFileAction.OverwriteSilently);
+                    }
+
                 }
             }
         }
 
-        private static void Unzip(string filename)
-        {
-        }
     }
 }
